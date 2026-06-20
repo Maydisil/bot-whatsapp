a87762df462f9d5be502ebee1c6e6c42f5d5659d
Maydisil <maydsondinael@hotmail.com>
2026-06-20 19:05:59 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Bot online!');
+});
+
+app.post('/enviar', (req, res) => {
+  const { mensagem } = req.body;
+
+  console.log('Mensagem recebida:', mensagem);
+
+  res.json({
+    sucesso: true,
+    recebido: mensagem
+  });
 });
 
 app.listen(PORT, () => {
